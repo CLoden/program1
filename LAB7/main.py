00e5cb18dda070cf38f3c2f2dffc3152550c3cb7
CLoden <77069749+CLoden@users.noreply.github.com>
2022-03-15 14:28:19 -0700

    Add files via upload

---
+++ LAB7/main.py
@@ -0,0 +1,43 @@
+from names import name
+
+def name_search_db(name):
+    while True:
+        try:
+            name_query = input("Enter the name you're searching for: ")
+            if name_query not in name:
+                print("Name not found in database. Please try again.")
+                name_search = input("Enter the name you're searching for: ")
+            else:
+                for name_query in name.readNames():
+                    print(name_query["Name"])
+        except:
+            print("Invalid input")
+
+def gender_search_db(name):
+    gender_query = ''
+    gender_query = input("Enter the gender you're searching for. Either M or F: ")
+    gender_query = gender_query.upper()
+    while gender_query != 'M' and gender_query != 'F':
+        print("Please enter M or F for gender.")
+        gender_query = input("Enter the gender you're searching for. Either M or F: ")
+        gender_query = gender_query.upper()
+    else:
+        for gender_query in name.readNames(Gender):
+            print(gender_query["Gender"])
+
+
+
+def main():
+
+    gender_search_db(name)
+    name_search_db(name)
+
+
+main()
+
+#Your main method should ask the user to enter values for both of your search parameters,
+# and use the readNames() method of Names.py to fetch the matching Name objects. Then, it
+# should write those objects to the console just as in the previous lab.
+
+# collecting inputs
+
---
+++ LAB7/names.py
@@ -0,0 +1,47 @@
+from database import Database
+
+class name:
+    __name = ""
+    __year = ""
+    __gender = ""
+    __count = ""
+
+    def __init__(self, name, year, gender, count):
+        self.__name = name
+        self.__year = year
+        self.__gender = gender
+        self.__count = count
+
+    def getName(self):
+        return self.__name
+
+    def getYear(self):
+        return self.__year
+
+    def getGender(self):
+        return self.__gender
+
+    def getCount(self):
+        return self.__count
+
+    def setName(self, newName):
+        self.__name = newName
+
+    def setYear(self, newYear):
+        self.__year = newYear
+
+    def setGender(self, newGender):
+        self.__gender = newGender
+
+    def setCount(self, newCount):
+        self.__count = newCount
+
+    #Your Name.py file should include a static method called readNames()
+    # that takes two parameters, and passes those parameters into the readNames() method in Database.py.
+    # It should return the list of names that it gets from readNames() in Database.py,
+    # just like in the previous assignment.
+    @staticmethod
+    def readNames(name, gender):
+        for row in Database.readNames(name,gender):
+            return name(row['name'], row['year'], row['gender'], row['count'])
+
